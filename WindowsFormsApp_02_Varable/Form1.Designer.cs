3fcc8923bc837494cb8a814de19fbb25e5f635ba
r2d2c2 <r2d2b22@gmail.com>
2025-04-28 11:24:37 +0900

    실습2 update

--- WindowsFormsApp_02_Varable/Form1.Designer.cs
+++ WindowsFormsApp_02_Varable/Form1.Designer.cs
@@ -28,39 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox_print = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_print1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox_print
+            // textBox_print1
             // 
-            this.textBox_print.Location = new System.Drawing.Point(38, 63);
-            this.textBox_print.Multiline = true;
-            this.textBox_print.Name = "textBox_print";
-            this.textBox_print.Size = new System.Drawing.Size(720, 344);
-            this.textBox_print.TabIndex = 0;
-            this.textBox_print.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox_print1.Location = new System.Drawing.Point(36, 70);
+            this.textBox_print1.Multiline = true;
+            this.textBox_print1.Name = "textBox_print1";
+            this.textBox_print1.Size = new System.Drawing.Size(1005, 521);
+            this.textBox_print1.TabIndex = 0;
+            this.textBox_print1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
             // 
-            // label1
+            // textBox2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(33, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 30);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Result:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.textBox2.Location = new System.Drawing.Point(1056, 70);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(266, 239);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1056, 329);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(266, 239);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox1_TextChanged_2);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox_print);
+            this.ClientSize = new System.Drawing.Size(1334, 777);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox_print1);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +75,9 @@
 
         private System.Windows.Forms.TextBox textBox_print;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_print1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- WindowsFormsApp_02_Varable/Form1.cs
+++ WindowsFormsApp_02_Varable/Form1.cs
@@ -52,8 +52,103 @@ namespace WindowsFormsApp_02_Varable
             double spaceToMoon_km;
             decimal myDecimal;
 
-            textBox_print.Text = $"{retroColorRed.GetType()} retroColorRed: {retroColorRed}";
-            textBox_print.Text += $"\r\n{distanceToCanada_cm.GetType()} distanceToCanada_cm: {distanceToCanada_cm}";
+            textBox_print1.Text = $"{retroColorRed.GetType()} retroColorRed: {retroColorRed}";
+            textBox_print1.Text += $"\r\n{distanceToCanada_cm.GetType()} distanceToCanada_cm: {distanceToCanada_cm}";
+            textBox_print1.Text += "\r\n";
+            int a = 5;
+            int b = 2;
+            textBox_print1.Text += a.ToString();
+            textBox_print1.Text += "\r\n";
+            textBox_print1.Text += $"{a} + {b} = {a + b} \r\n";
+            textBox_print1.Text += $"{a} - {b} = {a - b}\r\n";
+            textBox_print1.Text += $"{a} * {b} = {a * b}\r\n";
+            textBox_print1.Text += $"{a} / {b} = {a / b}\r\n";
+            textBox_print1.Text += $"{a} % {b} = {a % b}\r\n";
+            textBox_print1.Text += $"{a}pow{b}={Math.Pow(a, b)}\r\n";//a의 b제곱
+            textBox_print1.Text += $"{Math.Sqrt(9)}\r\n";//루트
+
+            textBox_print1.Text += $"{1 == 1}\r\n";
+            textBox_print1.Text += $"{1 == 2}\r\n";
+            textBox_print1.Text += $"{2 >= 2}\r\n";
+            textBox_print1.Text += $"{2 >= 1}\r\n";
+            textBox_print1.Text += $"{2 <= 2}\r\n";
+
+            textBox_print1.Text += $"\r\n";
+
+            textBox_print1.Text += $"{!true}\r\n";
+            textBox_print1.Text += $"{!!true}\r\n";
+            textBox_print1.Text += $"{!!!true}\r\n";
+            textBox_print1.Text += $"{!!!!true}\r\n";
+            textBox_print1.Text += $"\r\n";
+            textBox_print1.Text += $"{true&&true}\r\n";
+            textBox_print1.Text += $"{true&&false}\r\n";
+            textBox_print1.Text += $"{false && false}\r\n";
+            textBox_print1.Text += $"\r\n";
+            textBox_print1.Text += $"{true||true}\r\n";
+            textBox_print1.Text += $"{true||false}\r\n";
+            textBox_print1.Text += $"{false||false}\r\n";
+            textBox_print1.Text += $"\r\n";
+
+            int num = 10;
+            int num2 = 10;
+            int result1 = num++; //주의 대입 안해도 num 값이 오른다
+            int result2 = ++num2;
+            textBox_print1.Text = $"{result1}\r\n";
+            textBox_print1.Text += $"{result2}\r\n";
+
+            textBox_print1.Text = $"{num+=1}\r\n";//12
+            textBox_print1.Text = $"{num-=1}\r\n";//11
+            textBox_print1.Text += $"{num2 *=num2}\r\n";//121
+            textBox_print1.Text += $"{num2 /= num2}\r\n";//
+
+            //실습 2
+            // q1
+            //가격
+            int price = 1000;
+            //수량
+            int count = 5;
+            // 과일
+            string fruit = "사과";
+            textBox_print1.Text = $"{fruit} {count}개의 총 가격은 {price * count}원입니다.\r\n";
+
+            // q2
+            string strInt = "15";
+            int myInt2 = 10;
+            textBox_print1.Text = $"{strInt}에 {myInt2}를 더하면 {Convert.ToInt32(strInt) + myInt2}";
+
+            // q3
+            // 상품명
+            string productName = "노트북";
+            // 가격
+            int productPrice = 1000000;
+            //할인율
+            float discountRate = 0.1f;
+            // 재고 수량
+            byte stockCount = 10;
+            bool isAvailable = false;
+            //할인된 가격
+            double discountedPrice = productPrice * (1 - discountRate);
+            if(studentCount > 0)
+            {
+                isAvailable = true;
+                textBox_print1.Text=$"구매 가능: 할인 가격은 {discountedPrice:F2}입니다.\r\n";//실수 2자리 까지만 출력
+            }
+            else
+            {
+                isAvailable = false;
+                textBox_print1.Text = $"품절되었습니다.\r\n";
+            }
+            if (studentCount >= 5)
+            {
+                textBox2.Text = "여유 있음";
+            }
+            else
+            {
+                textBox2.Text = "소량 남음";
+            }
+            textBox3.Text = $"상품명:{productName},할인된 가격:{discountedPrice:F2}원, 재고:{studentCount}개";
+            
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -62,6 +157,16 @@ namespace WindowsFormsApp_02_Varable
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_2(object sender, EventArgs e)
         {
 
         }
